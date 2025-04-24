34c4a3a86af1857f26932dfbc43be0a9412e3254
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-24 21:00:11 +0200

    feat: main App file

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Routes, Route, Navigate } from 'react-router-dom';
+
+
+const App=()=>{
+    return(
+        <>
+            APP
+        </>
+    )
+}
+
+export default App
