99f6e7c45f565d9e3a727df6354a4a934f6210ba
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-24 17:18:24 +0200

    feat: login implemented

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import jwt from "jsonwebtoken";
 
 
 export const register = async (req, res) => {
@@ -22,4 +23,29 @@ export const register = async (req, res) => {
         res.status(500).json({message:"Server Error:", err})
     }
 
+}
+
+
+export const login=async(req,res)=>{
+    const {username,email,password}=req.body
+    try{
+        const user=await User.findOne({email})
+        if(!user)
+            return res.status(400).json({message:"No such user"});
+
+        const isMatch=user.comparePassword(password)
+        if(!isMatch)
+            return res.status(400).json({message:"Wrong Password"});
+
+        const token=jwt.sign(
+            {id:user._id,username:user.username,email:user.email},
+            process.env.JWT_SECRET,
+            {expiresIn:'1h'}
+        )
+        res.status(300).json({token})
+    }
+    catch(err){
+        console.error('Error during login:', err)
+        res.status(500).json({message:"Error server:",err})
+    }
 }
