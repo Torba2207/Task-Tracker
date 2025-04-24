406fbcc52f332a38e1b8b254c92fc78b75df74be
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-24 16:15:24 +0200

    upd: from cjs to mjs

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-const express=require('express');
-const cors=require('cors');
-const bodyParser=require('body-parser');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const uri = "mongodb+srv://Torba2207:Levis_2009@task-tracker-db.tqcka2w.mongodb.net/?retryWrites=true&w=majority&appName=task-tracker-db";
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-const PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
