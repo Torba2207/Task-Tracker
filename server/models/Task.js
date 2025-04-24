8bf0326ca76dbde02bcc6316eb24412fc017044f
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-24 18:18:07 +0200

    feat: Task Schema

---
+++ server/models/Task.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import User from "./User";
+
+const taskSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required: true,
+    },
+    dueDate:{
+        type: Date,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['pendign', 'in-progress','completed'],
+        default:'pending'
+    }
+
+},{
+    timestamps:true
+})
+
+
+const Task=mongoose.model('Task',taskSchema);
+export default Task;
