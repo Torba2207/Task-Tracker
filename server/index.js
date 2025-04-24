c345c134fb87870ecc0c6baa7cb025ac181660e7
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-24 16:31:36 +0200

    feat: Server is connected to Database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://Torba2207:Levis_2009@task-tracker-db.tqcka2w.mongodb.net/?retryWrites=true&w=majority&appName=task-tracker-db";
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+        console.log("Successful connection to Database")
+    })
+    .catch((err)=>{
+        console.error("Connection to Database has been failed:",err)
+    })
 
 app.get('/', (req, res) => {
     res.send('Server is running');
